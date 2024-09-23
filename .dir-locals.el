76ae374e467763fd6edf11d82f9d98d9795cb0ee
Vaarnan Drolia <vaarnan@gmail.com>
2024-09-23 18:37:43 +0100

    fix broken social links

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,9 +2,9 @@
 <table id='contact-header'>
   <tr>
     <td id='contact-right'>
-      <a href='https://twitter.com/vaarnan'><img class='social-icon' alt='social-link-twitter' src='img/twitter-circle.svg'>twitter</a>
-      <a href='https://github.com/vellvisher'><img class='social-icon' alt='social-link-github' src='img/github.svg'>github</a>
-      <a href='https://www.linkedin.com/in/vaarnan'><img class='social-icon' alt='social-link-linkedin' src='img/linkedin.svg'>linkedin</a>
+      <a href='https://twitter.com/vaarnan'><img class='social-icon' alt='social-link-twitter' src='/img/twitter-circle.svg'>twitter</a>
+      <a href='https://github.com/vellvisher'><img class='social-icon' alt='social-link-github' src='/img/github.svg'>github</a>
+      <a href='https://www.linkedin.com/in/vaarnan'><img class='social-icon' alt='social-link-linkedin' src='/img/linkedin.svg'>linkedin</a>
     </td>
   </tr>
 </table>"))))))
